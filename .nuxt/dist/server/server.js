5c40c8316fc8c21a896a9fc155c2cd5a6c1b74f6
Samael Aponte <samaelaponte@akimbo.com.co>
2021-07-10 11:08:47 -0400

    generatedFiles

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -845,7 +845,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: false,
+      isStatic: true,
       isDev: false,
       isHMR: false,
       app,
@@ -2148,6 +2148,10 @@ const layouts = {
 
     isFetching() {
       return this.nbFetching > 0;
+    },
+
+    isPreview() {
+      return Boolean(this.$options.previewData);
     }
 
   },
